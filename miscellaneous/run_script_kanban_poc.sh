53b65758e43c539fc787f24e4ab608edc66f3581
Todor Milev <todor.milev@gmail.com>
2018-07-17 11:26:11 -0400

    Printing logs to console.

--- miscellaneous/run_script_kanban_poc.sh
+++ miscellaneous/run_script_kanban_poc.sh
@@ -1,1 +1,1 @@
-../kanban-poc/src/fabcoind -gen --testkanban -logips -datadir=/home/$USER/.kanban/ -peers=3 -testdata=10 -addnode=18.130.88.126
+../kanban-poc/src/fabcoind -gen --testkanban -printtoconsole -logips -datadir=/home/$USER/.kanban/ -peers=3 -testdata=10 -addnode=18.130.88.126 
